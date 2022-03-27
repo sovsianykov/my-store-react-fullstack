32941fc8f5c1cb50dd02877434a820a4aaba1e91
”sovsianykov” <swebproger@gmail.com>
2022-03-27 14:29:46 +0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const routes = require('./routes/productRoutes')
 const connectDB = require('./db.js')
-const path =require('path')
 const cors = require('cors')
 
 const port = process.env.PORT || 8000
@@ -10,9 +9,7 @@ const app = express()
 app.use(cors({
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
-app.use(express.static('client/build'));
 
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
 connectDB()
 
